0fe66b1d07f3498e282cc2b9768b1c9c58033847
MattPlays <markgpmcpe@gmail.com>
2021-09-12 17:03:02 -0400

    axios rewrite

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const fetch = require("node-fetch");
+const axios = require("axios").default;
 const fs = require("fs");
 /**
  * @param {number} width - The width of the placeholder image you want to request
  * @param {number} height - The height of the placeholder image you want to request
  * @param {string} downloadPath - The location you want the placeholder image to be downloaded
- * @returns {Promise<void>} If successful this function will return void
+ * @returns {Promise<fs.WriteStream>}
  * @example
  * const placebear = require("placebear.js")
  * placebear(1920,1080, "path/to/file.jpg")
  */
 module.exports = async(width, height, downloadPath) => {
     const url = `https://placebear.com/${width}/${height}`;
-    return fetch(url, {
-        "method": "GET"
-    }).then((data) => {
-        return data.body.pipe(fs.createWriteStream(downloadPath));
+    return axios({
+        method: "GET",
+        url: url,
+        responseType: "stream"
+    }).then(({data}) => {
+        return data.pipe(fs.createWriteStream(downloadPath));
     }).catch((err) => {throw new Error(err)});
 }
